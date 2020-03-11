ee09b82545f61b26429713f63819592c8a02f0a1
J. Peter Gogarten <12535956+Gogarten-Lab@users.noreply.github.com>
2020-03-11 16:59:13 -0400

    Update rename_NCBI_fasta_to_nn_SeqSpecSpace.pl

--- rename_NCBI_fasta_to_nn_SeqSpecSpace.pl
+++ rename_NCBI_fasta_to_nn_SeqSpecSpace.pl
@@ -41,8 +41,7 @@ while ( my $line = <IN> ) {
 		$species_strain =~ s/\(/_/g;
 		$species_strain =~ s/\)/_/g;
 		$species_strain =~ s/\:/_/g;
-       # $species_strain =~ s/:/_/g;
-		#print "$species_strain\n";
+       
 		
 		#print OUT (">"."$count"."_"."$species_strain"." "."$line\n"); #this guaranties unique names
 		if ($dummy == 0)
